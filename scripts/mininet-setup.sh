7bd743e21e2c04ba2a72610ffcc810d908557fd1
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-02-28 14:53:22 +0100

    Removed comments and added shebangs in scripts

--- scripts/mininet-setup.sh
+++ scripts/mininet-setup.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
+
 mkdir mininet; curl -sL https://github.com/mininet/mininet/archive/master.tar.gz | tar xz -C mininet --strip=1
 
 mininet/util/install.sh -nfv
-
--- scripts/ryu-setup.sh
+++ scripts/ryu-setup.sh
@@ -1,4 +1,5 @@
-# sudo apt-get install -y gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev python-pip python-networkx
+#!/bin/bash
+
 sudo apt-get --no-install-recommends install -y python3-networkx
 
 mkdir ryu; curl -sL https://github.com/faucetsdn/ryu/archive/master.tar.gz | tar xz -C ryu --strip=1
@@ -6,4 +7,3 @@ cd ryu; pip3 install .
 cd ..
 
 mkdir flowmanager; curl -sL https://github.com/martimy/flowmanager/archive/master.tar.gz | tar xz -C flowmanager --strip=1
-
